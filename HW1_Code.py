60aa287549a0287291f4f0be25df0da43495280d
OnetoTommy <xingguangc09@gmail.com>
2024-11-22 12:46:21 -0800

    Demo
    Updated

--- HW1_Code.py
+++ HW1_Code.py
@@ -1,1 +1,9 @@
 import pandas as pd
+
+
+## Define hyper-parameter ranges for D-tree
+min_impurity_thr = [0.001,0.0001]
+min_samples_split_thr = [5,10]
+max_depth_thr = [3,5]
+min_samples_leaf_thr = [3,5]
+ccp_thr = [0.001,0.0001]
